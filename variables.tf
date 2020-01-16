a6ce005aab2057139af84f0fbb00970bc52bb595
Joachim Fischer <joachim.fischer@de.ibm.com>
2020-01-16 12:34:42 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -55,3 +55,19 @@ variable "server-count" {
 variable "ibmcloud_timeout" {
    default = 300
 }
+#-------------------- updaten ------------------------
+variable "imagename" {
+  description = "Name of the image key to be used"
+}
+
+variable "powerinstanceid" {
+  description = "Power Instance associated with the account"
+}
+
+variable "instancename" {
+  description = "Name of the instance"
+}
+
+variable "sshkeyname" {
+  description = "Name of the ssh key to be used"
+}
