23ed865862eda7711924d505b5d1cbacf29c4845
Joachim Fischer <joachim.fischer@de.ibm.com>
2020-01-16 12:26:31 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+# variables supplied from terraform.tfvars
+
+provider "ibm" {
+  ibmcloud_api_key        = "${var.ibmcloud_api_key}"
+  ibmcloud_timeout        = "${var.ibmcloud_timeout}"
+  region                  = "${var.ibmcloud_region}"
+}
